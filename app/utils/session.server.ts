5ea3e887ab84dbdaffcfcdb3a9c37971bc902499
Kenny <k6alvarez@gmail.com>
2021-11-25 21:51:39 -0800

    session and logout implemented

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -33,7 +33,7 @@ if (!sessionSecret) {
 
 let storage = createCookieSessionStorage({
   cookie: {
-    name: "KA_DOTCOM_session",
+    name: "RJ_session",
     secure: true,
     secrets: [sessionSecret],
     sameSite: "lax",
@@ -42,6 +42,59 @@ let storage = createCookieSessionStorage({
     httpOnly: true
   }
 });
+
+export function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request: Request) {
+  let session = await getUserSession(request);
+  let userId = session.get("userId");
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  let session = await getUserSession(request);
+  let userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    let searchParams = new URLSearchParams([
+      ["redirectTo", redirectTo]
+    ]);
+    throw redirect(`/login?${searchParams}`);
+  }
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  let userId = await getUserId(request);
+  if (typeof userId !== "string") {
+    return null;
+  }
+
+  try {
+    let user = await db.user.findUnique({
+      where: { id: userId }
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  let session = await storage.getSession(
+    request.headers.get("Cookie")
+  );
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session)
+    }
+  });
+}
 
 export async function createUserSession(
   userId: string,
