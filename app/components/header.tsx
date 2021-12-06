b50c525cc34ce21c259aaf0b510e7db2d6b33ed4
Kenny <k6alvarez@gmail.com>
2021-12-06 14:35:39 -0800

    update link styles

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,13 +6,22 @@ export const Header = () => {
   return (
     <header className="app-header">
       <h1>
-        <CacutsIcon color="hsla(15, 86%, 53%, 1)" />
+        <CacutsIcon color="hsla(112, 49%, 81%, 1)" />
         Kenny Alvarez
       </h1>
       <nav>
         <ul>
+          {/* <li>
+            <Link to="jokes">Photos</Link>
+          </li> */}
+          {/* <li>
+            <Link to="jokes">Code</Link>
+          </li>
           <li>
-            <Link to="jokes">Read Jokes</Link>
+            <Link to="jokes">Blog</Link>
+          </li> */}
+          <li>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
